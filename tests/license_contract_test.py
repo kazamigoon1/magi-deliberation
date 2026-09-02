15f636ae4283c75ecfe9d8d64afcd37556cb0173
허진수 <jsheo@simplatform.com>
2026-09-02 11:01:28 +0900

    Use GitHub nickname in MIT copyright notice

--- tests/license_contract_test.py
+++ tests/license_contract_test.py
@@ -6,6 +6,7 @@ license_text = (root / "LICENSE").read_text(encoding="utf-8")
 readme = (root / "README.md").read_text(encoding="utf-8")
 
 assert "MIT License" in license_text
+assert "Copyright (c) 2026 kazamigoon1" in license_text
 assert "Permission is hereby granted" in license_text
 assert "## License" in readme
 assert "MIT" in readme
